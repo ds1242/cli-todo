118c75c36a614ccc622c6258610e985469437e4b
David Shaw <dshaw11@utk.edu>
2024-08-06 15:25:01 -0400

    created a func to check for length to assign id correctly and then append to file with data

--- add_row.go
+++ add_row.go
@@ -5,30 +5,64 @@ import (
 	"errors"
 	"os"
 	"strconv"
+	"bufio"
 	"time"
+	"fmt"
 )
 
 func addRow(value string) error {
-	file, err := os.Open("data.csv")
+	file, err := os.OpenFile("data.csv", os.O_APPEND|os.O_WRONLY, 0644)
 	if err != nil {
 		return errors.New("unable to open data.csv")
 	}
-
-	filedata, _ := csv.NewReader(file).ReadAll()
 
 	defer file.Close()
 	writer := csv.NewWriter(file)
 	defer writer.Flush()
 	
-	id := strconv.Itoa(len(filedata))
+	id, err := generateId("data.csv")
+	if err != nil {
+		return errors.New("error getting length of file")
+	}
+
 	createdAt := time.Now().UTC
 	completed := "false"
 
 	rowToAdd := []string{id, value, createdAt().String(), completed }
-	if err := writer.Write(rowToAdd); err != nil {
-		
-		return errors.New("error writing record to file")
+	
+	writeErr := writer.Write(rowToAdd)
+	if writeErr != nil {
+		return writeErr
 	}
 
 	return nil
 }
+
+func generateId(filePath string) (string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return "", errors.New("unable to open data.csv")
+	}
+
+	defer file.Close()
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+	
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
+
+	// getting the number of lines
+	filedata, err := csv.NewReader(file).ReadAll()
+    if err != nil {        
+        return "", err
+    }
+
+	lineCount := len(filedata)
+	fmt.Println("Number of lines in file:", lineCount)
+	idVal := lineCount + 1
+
+	id:= strconv.Itoa(idVal)
+
+	return id, nil
+}
+
