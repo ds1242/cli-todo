67fb1dead5bd6bf10da710154a705a866b635a48
David Shaw <dshaw11@utk.edu>
2024-08-07 13:33:43 -0400

    finished coverting to a map and deleting the record from the map

--- delete_row.go
+++ delete_row.go
@@ -10,17 +10,28 @@ import (
 
 
 func deleteRecord(recordId string)(string, error) {
+	// convert input from user to get the record id as an int
 	recordIdInt, recordIdErr:= strconv.Atoi(recordId)
 	if recordIdErr != nil {
 		return "", errors.New("unable to parse record id")
 	}
-	var recordStructSlice []Row
+
+	mapOfRows := make(map[int]Row)
 	records := GetRecords("data.csv", ",")
-	fmt.Println(recordIdInt)
-	// fmt.Println(records)
-	for _, record := range records {
-		recordStructSlice = append(recordStructSlice, convertSliceOfRowsToRowStructs(record))
+
+	// Load map with row objects
+	for i, record := range records {
+		mapOfRows[i] = convertSliceOfRowsToRowStructs(record)
 	}
-	fmt.Println(recordStructSlice)
-	return "Deleted", nil
+	
+	for _, row := range mapOfRows {
+		if row.ID == recordIdInt {
+			delete(mapOfRows, row.ID)
+			return "Deleted record", nil
+		}
+	}
+
+	fmt.Println(mapOfRows)
+
+	return "no record to delete", nil
 }
