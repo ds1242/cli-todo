5ec13e8a256b48f183841dba4f916e122e81f302
David Shaw <dshaw11@utk.edu>
2024-08-09 12:17:03 -0400

    setup list command to display all tasks currently set

--- list_rows.go
+++ list_rows.go
@@ -8,17 +8,19 @@ import (
 func listTasks() error {
 	tasks := GetRecords("data.csv", ",")
 
-	fmt.Println("==================================================")
-	fmt.Println("| ID |     Task      |   Created At  |   Status  |")
+	fmt.Println("===============================================================================================")
+	fmt.Println("| ID  |        Task           |           Created At               |   Status  |")
+	count := 0
 	for _, task := range tasks {
-		for _, col := range task {
-			fmt.Println("--------------------------------------------------")
-			fmt.Printf("|  |     %v      |   %v |   %v  |\n", col[0], col[1], col[2])
-			
-		}
+		// for _, col := range task {
+			fmt.Println("-----------------------------------------------------------------------")
+			fmt.Printf("|  %d  |     %s      |   %s   |   %s  |\n", count, task[0], task[1], task[2])
+			// fmt.Printf(col[0])
+			count++
+		// }
 		
 	}
-	fmt.Println("==================================================")
+	fmt.Println("===============================================================================================")
 
 	return nil
 }
