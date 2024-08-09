3b9885f7576723cb1b210d0937934526c0041190
David Shaw <david.shaw1242@gmail.com>
2024-08-08 20:51:43 -0400

    more progress on setting up the list

--- list_rows.go
+++ list_rows.go
@@ -7,7 +7,18 @@ import (
 
 func listTasks() error {
 	tasks := GetRecords("data.csv", ",")
-	fmt.Println(tasks)
+
+	fmt.Println("==================================================")
+	fmt.Println("| ID |     Task      |   Created At  |   Status  |")
+	for _, task := range tasks {
+		for _, col := range task {
+			fmt.Println("--------------------------------------------------")
+			fmt.Printf("|  |     %v      |   %v |   %v  |\n", col[0], col[1], col[2])
+			
+		}
+		
+	}
+	fmt.Println("==================================================")
 
 	return nil
 }
